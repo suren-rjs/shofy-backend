c4521ca5ac6b2085f6fbc99cac38001d1c4f90e7
Surendiran <suren.rjs@gmail.com>
2024-03-08 19:10:35 +0530

    CORS Test

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const cloudinaryRoutes = require("./routes/cloudinary.routes");
 
 // middleware
 const corsOptions = {
-  origin: "*",
+  origin: "https://shopify-eta-flame.vercel.app",
 };
 
 app.use(cors(corsOptions));
